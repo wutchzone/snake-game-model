a9db3f0111fd4e05dde06f02259bdc9a54e34dc7
Daniel Sedlak <daniel@sedlak.biz>
2018-06-12 20:49:06 +0200

    Initial Commit

---
+++ SnakeGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SnakeGame
+//
+//  Created by Daniel Sedlak on 12.06.18.
+//  Copyright © 2018 Daniel Sedlak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
